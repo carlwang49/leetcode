fc0d04b8871005fbe54700bf386a539508d30dd7
hywang <hywang@netdb.csie.ncku.edu.tw>
2023-10-27 21:15:28 +0800

    implement 443_string_compression

---
+++ 443_string_compression.py
@@ -0,0 +1,27 @@
+from typing import List
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        
+        i = 0
+        s = ""
+        while i < len(chars):
+            current_char = chars[i]
+            j = i + 1
+            count = 1
+            while j < len(chars):
+                if chars[j] == current_char:
+                    count += 1
+                    j += 1
+                else:
+                    break
+            s += chars[i]
+            if count > 1:
+                s += str(count)
+            i += count
+        
+        chars.clear()
+        for char in s:
+            chars.append(char)
+
+        return len(chars)
